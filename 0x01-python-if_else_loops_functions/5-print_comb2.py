cca13d267f7e5cbc7eac28cc5d485e11f91b5350
Lucia Rodriguez <774@holbertonschool.com>
2019-04-30 23:18:56 +0000

    [Update] Fixing prints again

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
+    if i != 99:
         print("0{:d}".format(i), end=", ")
-    elif i == 99:
-        print("{:d}".format(i))
     else:
         print("{:d}".format(i), end=", ")
