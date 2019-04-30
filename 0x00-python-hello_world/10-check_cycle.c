dcfc93d59fc4181f4703a50060c9cd196120c5b2
Lucia Rodriguez <774@holbertonschool.com>
2019-04-30 03:32:50 +0000

    [Update] Uploading the 10th task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ *check_cycle - checks if a single linked list contains a cycle
+ *@list: structure to be tested
+ *Return: 1 if a cycle is found
+ */
+int check_cycle(listint_t *list)
+{
+}
