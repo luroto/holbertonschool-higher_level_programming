40f23fdcf2aa671631bc431b00295bbfb809696c
Lucia Rodriguez <774@holbertonschool.com>
2019-05-02 18:38:18 +0000

    [Update] Trying something different

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    total = len(sys.argv)
+    from sys import argv
+    total = len(argv)
     if total == 1:
         print("0 arguments.")
     elif total == 2:
         print("1 argument:")
-        print("{:d}: {:s}".format((1), sys.argv[1]))
+        print("{:d}: {:s}".format((1), argv[1]))
     else:
         print("{:d} arguments".format(total - 1))
         for i in range(1, total):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
