7cc0cc3e59c685938bc5f45658e9ec9ab7b7589b
Lucia Rodriguez <774@holbertonschool.com>
2019-05-02 11:20:37 +0000

    [Update] Fixing the zero task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add as suma
+    from add_0 import add
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, suma(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
