9c51145c34486c44071ded5a0de8cbd1501aa829
Lucia Rodriguez <774@holbertonschool.com>
2019-05-03 00:32:39 +0000

    [Update] Fixing number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("{:d}: {:s}".format((1), argv[1]))
     else:
-        print("{:d} arguments:".format(total))
+        print("{:d} arguments:".format(total - 1))
         for i in range(1, total):
             print("{:d}: {:s}".format(i, argv[i]))
