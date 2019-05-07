3a089dc031c9516fabcd2ce2e684964d27b67d3d
Lucia Rodriguez <774@holbertonschool.com>
2019-05-07 20:30:45 +0000

    [Update] Uploading the second task

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     for i in range(len(my_list)):
         if idx < 0:
             return(my_list)
-        elif idx > len(my_list):
+        elif idx >= len(my_list):
             return(my_list)
         elif i == idx:
             my_list[i] = element
