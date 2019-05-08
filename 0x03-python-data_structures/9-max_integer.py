1c0585093b6abd2ddf362cb45392f07710f73f8c
Lucia Rodriguez <774@holbertonschool.com>
2019-05-08 01:31:01 +0000

    [Feature] Updating the 9th task

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is not None:
+        i = 0
+        for z in my_list:
+            if z > i:
+                i = z
+        return(i)
