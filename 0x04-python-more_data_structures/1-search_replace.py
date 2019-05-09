88f6037854896618e77952da348d046d548d9d50
Lucia Rodriguez <774@holbertonschool.com>
2019-05-09 23:46:14 +0000

    [Feature] Uploading the first task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list is None:
+        return(my_list)
+    else:
+        newlist = my_list.copy()
+        for i in range(len(my_list)):
+            if my_list[i] == search:
+                newlist[i] = replace
+            else:
+                newlist[i] = my_list[i]
+        return(newlist)
