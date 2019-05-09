8c49e335c00b05c62b9ee16aad9d914a5233243b
Lucia Rodriguez <774@holbertonschool.com>
2019-05-09 21:16:07 +0000

    Uploading the zero task

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        print()
+    else:
+        newmatrix = []
+        newmatrix = [[(row[i]**2)for i in range(len(matrix))] for row \
+                    in matrix]
+    return(newmatrix)
