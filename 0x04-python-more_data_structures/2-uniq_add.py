aadf6f19d60f41f7ae626478475e2b0a1fd73f80
Lucia Rodriguez <774@holbertonschool.com>
2019-05-11 22:49:27 +0000

    [Feature] Uploading the second task

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    if not my_list:
+        return(None)
+    else:
+        newlist = set(my_list)
+        suma = 0
+        for i in newlist:
+            suma += i
+        return(suma)
