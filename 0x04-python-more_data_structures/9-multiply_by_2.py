4a7dd0172282ba7eff2a6c89053f6738d6e3f0f3
Lucia Rodriguez <774@holbertonschool.com>
2019-05-12 02:02:27 +0000

    [Feature] Uploading the nineth task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    newdictionary = {}
+    newdictionary = dict(a_dictionary)
+    for k, v in newdictionary.items():
+        newdictionary[k] = v * 2
+    return(newdictionary)
