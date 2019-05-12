465927efc4a10c8e2f54c9c1aa5853e5b330ed45
Lucia Rodriguez <774@holbertonschool.com>
2019-05-12 01:03:17 +0000

    [Feature]Uploading the 8th task:

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary is True:
+        del a_dictionary[key]
+        return(a_dictionary)
+    else:
+        return(a_dictionary)
