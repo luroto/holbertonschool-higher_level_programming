e988b8094ec4cbb1b069cd09b67cec9d7f870e2a
Lucia Rodriguez <774@holbertonschool.com>
2019-05-20 23:49:29 +0000

    [Feature] Uploading the first advanced task

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except Exception as printing:
+        print("Exception: {}".format(printing), file=sys.stderr)
+        return(False)
+
+
