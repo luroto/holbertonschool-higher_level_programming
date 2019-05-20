eaf03d9039fc94abb2e1a021ee0aba3d2ef3b547
Lucia Rodriguez <774@holbertonschool.com>
2019-05-20 20:55:37 +0000

    [Feature] Uploading the second task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     suma = 0
-    for i in my_list:
+    for i in range(x):
         try:
-            if i <= x:
-                print("{:d}". format(i))
-                suma += 1
+            print("{:d}".format(my_list[i]), end="")
+            suma += 1
+        except TypeError:
+            pass
         except ValueError:
             pass
+    print()
     return(suma)
