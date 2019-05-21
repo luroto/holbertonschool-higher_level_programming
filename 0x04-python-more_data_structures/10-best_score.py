e721f80043ff35781fa508e08c1b09afbc803315
Lucia Rodriguez <774@holbertonschool.com>
2019-05-21 03:08:24 +0000

    [Feature] Uploading the 10th task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return(None)
+    else:
+        newdict = a_dictionary.items()
+        newdict = sorted(list(newdict))
+        return (newdict[0][0])
