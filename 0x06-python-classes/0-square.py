f3ce86c78c23c45ee8b255c85fdefc35dac14410
Lucia Rodriguez <774@holbertonschool.com>
2019-05-22 02:04:00 +0000

    [Update] Uploading the zero task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""This is the first documentation I've written for Holberton School """
+
+
+class Square:
+    """This is the class Square. At this moment I only need to create it"""
+    pass
+
+s = Square()
