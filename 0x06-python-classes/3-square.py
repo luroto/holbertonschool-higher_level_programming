6255f492d2e5df4b0283610e46ed43ad6069aa27
Lucia Rodriguez <774@holbertonschool.com>
2019-05-22 03:55:13 +0000

    [Update] Uploading the third task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This is the first project using Documentation for Python """
+
+
+class Square:
+    """On this task I'll set a size and dispose some exceptions"""
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return(self.__size**2)
