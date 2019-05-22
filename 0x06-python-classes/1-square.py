7e6b9a5024517436668241dbc152019ce586cc31
Lucia Rodriguez <774@holbertonschool.com>
2019-05-22 02:35:41 +0000

    [Update] Uploading the first task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""This is the first project using documentation for Python """
+
+
+class Square:
+    """I'll define a square using a size attribute"""
+    def __init__(self, size):
+        self.__size = size
