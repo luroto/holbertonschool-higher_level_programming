4964031b9f0b0b065e0ee0596af902edf6f68ccd
Lucia Rodriguez <774@holbertonschool.com>
2019-05-22 03:49:48 +0000

    [Update] Uploading the second file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""This is the first project using Documentation for Python """
+
+
+class Square:
+    """On this task I'll set a size and dispose some exceptions"""
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
