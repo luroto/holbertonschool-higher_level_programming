d22beab4b0414407fdea123baa3b2a544822a255
Lucia Rodriguez <774@holbertonschool.com>
2019-05-24 20:17:15 +0000

    [Update] Uploading with pep8 fixed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,6 +4,8 @@ Function cheatsheet:
 add_integer(4, 5)
 9
 """
+
+
 def add_integer(a, b=98):
     """ This function works only with integer and float numbers, any other
     type of argument will be re rejected"""
