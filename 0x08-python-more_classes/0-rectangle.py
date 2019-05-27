eb4d9274de56ece137dfd0ff94a4ee1000f65398
Lucia Rodriguez <774@holbertonschool.com>
2019-05-27 18:44:04 +0000

    [Feature]Uploading all the files

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a rectangle"""
+
+
+class Rectangle:
+    """This is the class Rectangle. For a while, just defining it is fine"""
+    def __init__(self):
+        pass
+
+r = Rectangle
