be33a766415348e4df93f8e61eadfac051978ac7
Lucia Rodriguez <774@holbertonschool.com>
2019-06-06 03:43:04 +0000

    [Update] Uploading the third file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is the documentation for this module. This time we have to add
+a string into a given file, no matter if file previously existed or not"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'r+') as archivo:
+        return(archivo.write(text))
