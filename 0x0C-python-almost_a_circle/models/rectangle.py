bdaf3323d0cbbe494f98764199adb9c55df1ff17
Lucia Rodriguez <774@holbertonschool.com>
2019-06-10 22:11:00 +0000

    [Update]Uploading the 8th task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/python3
+"""This module creates a rectangle"""
 from .base import Base
 
 
@@ -113,3 +114,18 @@ class Rectangle(Base):
         retos = (rec + parent + sid + pars + " " + equis + div + ygriega +
                  " " + "-" + " " + ancho + div + alto)
         return(retos)
+
+    def update(self, *args):
+        i = 0
+        for i in range(len(args)):
+            if i == 0:
+                self.id = args[i]
+            if i == 1:
+                self.__width = args[i]
+            if i == 2:
+                self.__height = args[i]
+            if i == 3:
+                self.__x = args[i]
+            if i == 4:
+                self.__y = args[i]
+            i += 1
