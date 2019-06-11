fa1811d8275588d93bd6e1b77a3db9a6cdb07b9a
Lucia Rodriguez <774@holbertonschool.com>
2019-06-11 02:07:25 +0000

    [Update] Uploading the 11th task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,12 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         Rectangle.__init__(self, size, size, x, y, id)
+
+    @property
+    def size(self):
+        return(self.width)
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
