1d538aaf117f20628dadbd091de30df110cee78f
Lucia Rodriguez <774@holbertonschool.com>
2019-06-11 22:16:26 +0000

    [Update] Fixing with comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,29 +5,12 @@ from .base import Base
 
 class Rectangle(Base):
     """ This is the class Rectangle, which inherits from base"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Class constructor for the Rectangle class"""
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = width
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = height
-        if type(x) != int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = x
-        if type(y) != int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = y
+        """ Class constructor """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
