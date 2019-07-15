6d6d4f637b3e45b9e207fa0f9fb6730cb02829e0
Lucia Rodriguez <774@holbertonschool.com>
2019-07-15 22:20:22 +0000

    [Update] Uploading the sixth seventh files

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
 -- This script adds data to a given table
 -- Trying INSERT TO
-USE hbtn_0c_0 IF EXISTS;
-INSERT INTO `first_table`(`id`,`name`) VALUES (89, "Holberton School");
+INSERT INTO first_table(`id`,`name`) VALUES (89, "Holberton School");
