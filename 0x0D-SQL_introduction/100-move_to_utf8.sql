49d5012cbcec3b29c950135236d4c68bfb17a794
Lucia Rodriguez <774@holbertonschool.com>
2019-07-16 01:16:54 +0000

    [Update] Uploading the 100th task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- This script converts databases into UTF-8 encoding
 -- This is pretty cool, actually
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR (256) CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8 COLLATE utf8_general_ci
+ALTER TABLE hbtn_0c_0.first_table MODIFY `name` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;
