3afa69bb426ad9f231bc1c6ddb7323186d95cad3
Lucia Rodriguez <774@holbertonschool.com>
2019-07-17 20:20:10 +0000

    [Update] Uploading the eleventh file

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,4 @@
 -- This script list all shows contained in the databes
-SELECT DISTINCT tv_shows.title,  tv_show_genres.genre_id FROM tv_shows, tv_show_genres
-LEFT JOIN tv_
-WHERE tv_shows.id = tv_show_genres.show_id OR tv_show_genres.genre_id IS NULL
+SELECT tv_shows.title,  tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
