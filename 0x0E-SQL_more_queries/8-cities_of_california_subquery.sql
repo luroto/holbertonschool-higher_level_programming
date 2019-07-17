5794d2aa73d122d9eb126f6b1e09e1c0426c5364
Lucia Rodriguez <774@holbertonschool.com>
2019-07-17 02:00:49 +0000

    [Update] Uploading the 8th task again

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- This script list all the ciites of California found in some database
 -- states_id must be used for searching
-SELECT `state_id`, `name` FROM cities
-WHERE `state_id` =
-  (SELECT `id` FROM states WHERE `name` = 'California')
+SELECT `id`, `name` FROM cities
+WHERE `state_id` = (SELECT `id` FROM states WHERE `name` = 'California')
 ORDER BY cities.id ASC;
