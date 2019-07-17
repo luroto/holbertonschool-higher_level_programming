6e2ec77016ad6da2cfb7608e4b90f30cb893f230
Lucia Rodriguez <774@holbertonschool.com>
2019-07-17 01:36:53 +0000

    [Update] Uploading the seventh file again, fixed primary keys

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,5 @@
 -- This script creates a database and a table if it doesn't exist
 -- This table will use a foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT, state_id INT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL, FOREIGN KEY (`state_id`) REFERENCES `states`(`id`));
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (`state_id`) REFERENCES `states`(`id`));
