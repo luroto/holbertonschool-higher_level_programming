f028b24ea0fe56fc730ff6ff2cb79829393cb914
Lucia Rodriguez <774@holbertonschool.com>
2019-07-17 00:32:09 +0000

    [Update] Uploading the second task

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- This script creates a database and an user
 -- User_0d_2 and database htbn_0d_2
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2` TO 'user_0d_2'@'localhost;
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
