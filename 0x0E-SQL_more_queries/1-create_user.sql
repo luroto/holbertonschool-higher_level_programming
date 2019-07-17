3f462889ada8fa846281d59392cebcd761db5102
Lucia Rodriguez <774@holbertonschool.com>
2019-07-17 00:27:07 +0000

    [Update] Uploading the first and the second files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- This script creates a server user with all privileges
 -- It shouldn't fail if the username already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'used_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
