b668a1ad8e3d646fec2cf78b26d56a773db0f90f
Lucia Rodriguez <774@holbertonschool.com>
2019-07-17 00:41:26 +0000

    [Update] Uploading the fourth task

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- This script creates a table with some criteria on this field
+-- It will use a default value on one of its fields
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
