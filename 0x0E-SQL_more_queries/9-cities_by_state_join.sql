57ab8ef0eeba7f2189144519e27640e0a180354c
Lucia Rodriguez <774@holbertonschool.com>
2019-07-17 03:21:41 +0000

    [Update] Uploading the 9th task again

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- This script lists all cities contained in some database using some criteria
 -- Only one SELECT statement
-SELECT cities.id, cities.name, states.name from cities
-INNER JOIN states ON cities.id = states.id
+SELECT cities.id, cities.name, states.name FROM cities
+INNER JOIN states
+WHERE cities.id = states.id
 ORDER BY cities.id ASC;
