47a50f88246c43c850e4f8affc147fd2814cd75c
Lucia Rodriguez <774@holbertonschool.com>
2019-07-18 00:12:29 +0000

    [Update] Uploading the 12th task

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,4 @@
 -- This script lists all the shows which doesn't have a linked genre
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id where tv_show_genres.show_id IS NULL;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.show_id IS NULL;
