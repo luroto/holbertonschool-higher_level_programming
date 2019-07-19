f44c8337ec523dc232969453323ad947fbcbc957
Lucia Rodriguez <774@holbertonschool.com>
2019-07-19 21:30:50 +0000

    [Update] Uploading the 101th task

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,6 @@
 -- This script lists all the genres that are not linked with any shows
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
-WHERE tv_show_genres.show_id IS NULL
-GROUP BY genre
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title FROM tv_shows
+WHERE tv_shows.title NOT IN (SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy")
+ORDER BY tv_shows.title ASC;
