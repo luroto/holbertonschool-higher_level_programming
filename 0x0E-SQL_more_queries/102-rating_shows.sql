ff2925f07ee1dfec97ccaca70252a0bf91ac31d3
Lucia Rodriguez <774@holbertonschool.com>
2019-07-20 01:05:47 +0000

    [Update] Uploading the rating sums

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,5 @@
 -- This script lists all the genres that are not linked with any shows
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
-WHERE tv_show_genres.show_id IS NULL
-GROUP BY genre
-ORDER BY number_of_shows DESC;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_show_ratings LEFT JOIN tv_shows
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC, tv_shows.title ASC;
