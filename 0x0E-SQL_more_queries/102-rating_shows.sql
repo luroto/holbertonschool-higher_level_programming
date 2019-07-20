208dad4429acb087568fd41a62967bc37f6ae298
Lucia Rodriguez <774@holbertonschool.com>
2019-07-20 01:07:33 +0000

    [Update] Uploading the 102th file

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_show_ratings LEFT JOIN tv_shows
 ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
-ORDER BY rating DESC, tv_shows.title ASC;
+ORDER BY rating DESC;
