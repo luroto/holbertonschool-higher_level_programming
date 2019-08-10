086471f04794571d23e1fb36455f18d954a92d9e
Lucia Rodriguez <774@holbertonschool.com>
2019-08-10 16:25:45 -0500

    [Fix] added binary like

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     cura = db.cursor()
     cura.execute("""SELECT *
                  FROM states
-                 WHERE states.name LIKE 'N%'
+                 WHERE states.name LIKE BINARY 'N%'
                  ORDER BY states.id ASC
                  """)
     catchi = cura.fetchall()
