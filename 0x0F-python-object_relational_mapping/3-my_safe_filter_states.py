c472fd0eb8cca12b231b4e419c3b5c98e18fc883
Lucia Rodriguez <774@holbertonschool.com>
2019-08-10 16:42:50 -0500

    [Feature] Uploading the third task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+""" This module connects to a database and lists the matching
+rows given some inputs. I'll check if the triple """ """
+avoids some types of SQL injection"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host="localhost",
+            user=str(sys.argv[1]),
+            password=str(sys.argv[2]),
+            db=str(sys.argv[3]),
+            port=3306,
+        )
+    inpu = str(sys.argv[4])
+    cura = db.cursor()
+    cura.execute("""SELECT *
+                 FROM states
+                 WHERE states.name LIKE BINARY "{}"
+                 ORDER BY states.id ASC
+                 """.format(inpu))
+    catchi = cura.fetchall()
+    if catchi:
+        for it in catchi:
+            print("{}".format(it))
+    cura.close()
+    db.close()
