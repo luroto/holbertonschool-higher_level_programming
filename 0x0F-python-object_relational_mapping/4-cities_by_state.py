b7d85bdefe7dd6bc0400932956dd9e9a56f1b96f
Lucia Rodriguez <774@holbertonschool.com>
2019-08-10 19:08:02 -0500

    [Feature]Uploading the 4th task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" This script connects to a database and list its content based on a
+given field"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    databba = MySQLdb.connect(
+                host='localhost',
+                user=str(sys.argv[1]),
+                password=str(sys.argv[1]),
+                db=str(sys.argv[3]),
+                port=3306,
+                )
+    consulta = databba.cursor()
+    consulta.exec("""SELECT cities.id, cities.name, states.name
+                    FROM cities, states
+                    WHERE cities.state_id = states.id
+                    ORDER BY cities.id ASC""")
+    catchi = consulta.fetchall()
+    if catchi:
+        for element in catchi:
+            print({}.format(catchi))
+    consulta.close()
+    databba.close()
