d3c73c73b46f11f54989fefb7a993d782cdb1b3c
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-12 02:40:42 +0000

    [Feature] Uploading the model_state, the sql script and the python string which connects everything

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" This file contains the declaration of a class """
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ This section defines States"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, index=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
