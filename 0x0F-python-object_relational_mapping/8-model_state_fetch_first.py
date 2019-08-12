14b18ed79b4d25161cdc9b8df84adb0caf806ba3
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-12 04:08:06 +0000

    [Feature] Uploading the 8th task working

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" This script lists the first object from some database"""
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter_by(id=1).order_by(
+                State.id.asc()):
+        print("{}: {}".format(instance.id, instance.name))
+    session.close()
