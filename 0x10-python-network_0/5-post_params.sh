474c3902f7bfb0d3a344b2610c596844da3b915a
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-20 23:56:54 +0000

    [Fix] Reordering arguments

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request with some keys and values 
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com" -d "subject=I always be here for PLD" "$1"
