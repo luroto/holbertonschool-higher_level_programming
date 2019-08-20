7156ea0f5dfc68f69de2138fcc10e5dcff6caeed
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-20 22:24:45 +0000

    [Feature] Uploading the third file

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script lists all the available methods
-curl -s -i -X OPTIONS "$1" | grep "Allow:"| cut -d" "  -f 2-6 "
+curl -s -i -X OPTIONS "$1" | grep Allow: | cut -d" "  -f 2-6 
