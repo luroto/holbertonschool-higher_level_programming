8b77c79d9310bb19d3f692657b050ddbdc831278
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-26 23:42:24 +0000

    [Feature] Uploading the zero task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" This task is for exploring bytes methods and dict"""
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    fetching = response.read()
+print("Body response:")
+print("\t- type: {}".format(type(fetching)))
+print("\t- content: {}".format(fetching))
+print("\t- utf8 content: {}".format(fetching.decode('utf-8')))
