b6ae7b2de4a273f5b9c1cc14b92795de19639bee
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-27 03:59:26 +0000

    [Feature] Uploading the third task

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+This script sends an request and displays the body of the response (error)
+"""
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            fetchi = response.read()
+            print(fetchi.decode('utf-8'))
+    except urllib.error.HTTPError as dont:
+        print("Error code: {}".format(dont.code))
