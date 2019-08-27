319d420e94d47e54d2ce3b731467f46bf5da3327
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-27 04:34:25 +0000

    [Feature] Uploading the 7th task

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+This script sends an request and displays the body of the response (error)
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    try:
+        req = requests.get(url)
+        print(req.text)
+    except HTTPError as dont:
+        print("Error code: {}".format(dont.response.status_code))
