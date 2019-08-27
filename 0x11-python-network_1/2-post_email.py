e8f1d09eb14b92425cfcd9f6917987c2c61c736a
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-27 03:20:15 +0000

    [Feature] Uploading the second task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+This script sends a POST request to some URL
+"""
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
+    linkcito = sys.argv[1]
+    email = sys.argv[2]
+    valores = {'email': email}
+    data = urllib.parse.urlencode(valores)
+    data = data.encode('ascii')
+    req = urllib.request.Request(linkcito, data)
+    with urllib.request.urlopen(req) as response:
+        catching = response.read()
+        print(catching.decode('utf-8'))
