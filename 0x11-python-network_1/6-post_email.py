9e194b4913eac0b6195ceac1848a9148f84ca40b
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-27 04:26:49 +0000

    [Feature] Uploading the 6th task

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+This script sends a POST request to some URL
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+    linkcito = sys.argv[1]
+    email = sys.argv[2]
+    valores = {'email': email}
+    req = requests.post(linkcito, data=valores)
+    print(req.text)
