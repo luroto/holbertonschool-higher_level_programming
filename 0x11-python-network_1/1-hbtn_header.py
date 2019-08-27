325889471823787a17810c97ada7b53ca1601aee
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-27 02:51:29 +0000

    [Feature] Uploading the first task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+This script finds X-Requested-Id of a request
+"""
+import sys
+import urllib.request
+linkcito = sys.argv[1]
+req = urllib.request.Request(linkcito)
+with urllib.request.urlopen(req) as response:
+    fetching = response.read()
+dicto = dict(response.getheaders())
+print(dicto['X-Request-Id'])
