59cff53a70521d9197aa5ca4e7ace7f4506e98a7
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-27 03:02:47 +0000

    [Feature] Uploding twofiles

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ This task is for exploring bytes methods and dict"""
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    fetching = response.read()
-print("Body response:")
-print("\t- type: {}".format(type(fetching)))
-print("\t- content: {}".format(fetching))
-print("\t- utf8 content: {}".format(fetching.decode('utf-8')))
+if __name__ == __main__:
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        fetching = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(fetching)))
+    print("\t- content: {}".format(fetching))
+    print("\t- utf8 content: {}".format(fetching.decode('utf-8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,10 @@
 """
 This script finds X-Requested-Id of a request
 """
-import sys
-import urllib.request
-linkcito = sys.argv[1]
-req = urllib.request.Request(linkcito)
-with urllib.request.urlopen(req) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    linkcito = sys.argv[1]
+    req = urllib.request.Request(linkcito)
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get('X-Request-Id'))
