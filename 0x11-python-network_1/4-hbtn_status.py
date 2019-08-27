61764bd5164dd657c05135f3a71565658e3a9e54
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-27 04:14:00 +0000

    [Feature] Uploading the 4th task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" This task fetches an url using requests module"""
+if __name__ == "__main__":
+    import requests
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
