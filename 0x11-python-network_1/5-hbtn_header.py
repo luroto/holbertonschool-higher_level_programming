79cccf25c4c69d3287a455c39e79c8cae4944756
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-08-27 04:18:20 +0000

    [Feature] Uploading the fifth task

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+This script finds X-Requested-Id of a request
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+    linkcito = sys.argv[1]
+    req = requests.get(linkcito)
+    print(req.headers.get('X-Request-Id'))
