8a3d0d0253ec2300de01a80b7f9f6c459c828fcc
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-18 03:29:03 +0000

    [Fix] Uploading the third file

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-{
-  let counter = 0;
-  process.argv.forEach((val, index) => {
-    counter = index;
-  });
-  if (counter > 1) {
-    console.log(process.argv[2]);
-  }
-  if (counter === 1) {
-    console.log('No argument');
-  }
+let counter = 0;
+process.argv.forEach((val, index) => {
+  counter++;
+});
+if (counter > 2) {
+  console.log(process.argv[2]);
+}
+if (counter <= 2) {
+  console.log('No argument');
 }
