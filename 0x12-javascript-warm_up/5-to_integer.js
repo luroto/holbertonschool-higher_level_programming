431f3df4ef858f481b141efa7fc05ad4012cc5aa
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-18 04:13:11 +0000

    [Fix] Fixing style

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+
 if (isNaN(process.argv[2]) === true) {
   console.log('Not a number');
 } else {
-  console.log(`My number is ${process.argv[2]}`);
+  console.log('My number is' + ${process.argv[2]}`);
 }
