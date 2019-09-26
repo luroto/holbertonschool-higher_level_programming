964f18d1c4ad808f9ca44989986d078e24de0956
Lucia Rodriguez Toloza <774@holbertonschool.com>
2019-09-26 03:32:41 +0000

    [Feature] Uploading the second task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2],function (error, response, body) {
-  console.log('code:', response.statusCode);
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
-
